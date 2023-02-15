a5bfd17aeb9ec46bd6e5d87b41054fc8d9b5a929
migup <migupele@gmail.com>
2023-02-15 15:05:21 +0100

    Empezando

--- core/src/com/mygdx/game/screens/GameOverScreen.java
+++ core/src/com/mygdx/game/screens/GameOverScreen.java
@@ -3,16 +3,9 @@ package com.mygdx.game.screens;
 
 import static com.mygdx.game.extras.Utils.SCREEN_HEIGHT;
 import static com.mygdx.game.extras.Utils.SCREEN_WIDTH;
-import static com.mygdx.game.extras.Utils.WORLD_HEIGHT;
-import static com.mygdx.game.extras.Utils.WORLD_WIDTH;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.MainGame;
 import com.mygdx.game.extras.AssetMan;
 
@@ -29,7 +22,7 @@ public class GameOverScreen extends BaseScreen{
 
         this.fontCamera = (OrthographicCamera) this.stage.getCamera();
 
-        prepareMessage("Game over!\nTouch the \nscreen to \nstart again!");
+        prepareMessage("GAME OVER!\nTouch the \nscreen to \nstart again!");
 
         this.music = AssetMan.getInstance().getGOMusic();
     }
--- core/src/com/mygdx/game/screens/GetReadyScreen.java
+++ core/src/com/mygdx/game/screens/GetReadyScreen.java
@@ -2,16 +2,9 @@ package com.mygdx.game.screens;
 
 import static com.mygdx.game.extras.Utils.SCREEN_HEIGHT;
 import static com.mygdx.game.extras.Utils.SCREEN_WIDTH;
-import static com.mygdx.game.extras.Utils.WORLD_HEIGHT;
-import static com.mygdx.game.extras.Utils.WORLD_WIDTH;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.MainGame;
 import com.mygdx.game.extras.AssetMan;
 
