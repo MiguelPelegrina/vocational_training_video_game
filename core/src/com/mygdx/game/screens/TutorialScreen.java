9053eb51f80e6b14e24c5ab54581805548364675
migup <migupele@gmail.com>
2023-02-16 14:59:15 +0100

    Empezando

---
+++ core/src/com/mygdx/game/screens/TutorialScreen.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.audio.Sound;
+import com.mygdx.game.MainGame;
+
+/**
+ * Clase que mostrará un tutorial. Utilizaremos el estado del tutorial para mostrar los diferentes
+ * aspectos del juego: evitar enemigos, coleccionar manzanas y como desplazarse
+ */
+public class TutorialScreen extends BaseScreen{
+    // Atributos de la clase
+    private int state;
+    private Sound sound;
+
+    /**
+     * Constructor por parámetros
+     * @param mainGame
+     */
+    public TutorialScreen(MainGame mainGame) {
+        super(mainGame);
+        this.state = 0;
+
+        // TODO Sonido de Na'vo "Hey, listen!"
+    }
+}
